474843364060f40cefa179a2a04eebc75dec4aec
IEmen <admin@psyxor.cf>
2023-04-13 22:10:51 +0500

    Add static bootsrap 4, jquery 3.6.4. Navbar. Corrected select. Small db path fix for win32.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+#coding:utf8
+
+
 import sqlite3
+import os
+from sys import platform
+from flask import Flask, render_template
 
 app = Flask(__name__)
-DB = 'db/nettab.db'
-
+# Тут мелкая боль разработки под виндой, хех, путь коварен.
+if platform == 'win32':
+    DB = os.getcwd()+'\\db\\nettab.db'
+else:
+    DB = 'db/nettab.db'
 
 def get_addresses(db: str) -> list:
     '''
@@ -11,7 +19,15 @@ def get_addresses(db: str) -> list:
     '''
     connection = sqlite3.connect(db)
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM address')
+    cursor.execute("""SELECT address.root_to, address.city, address.street, address.building, type_channel.name, type_phys_channel.name
+from
+main, address, type_channel, type_phys_channel
+where
+main.address_id = address.id
+AND
+main.type_channel_id = type_channel.id
+AND
+main.type_phys_channel_id = type_phys_channel.id""")
     return [i for i in cursor]
 
 
@@ -19,3 +35,6 @@ def get_addresses(db: str) -> list:
 def index():
     addresses = get_addresses(DB)
     return render_template('index.html', addresses=addresses)
+
+
+app.run(host='127.0.0.1', port=8080)
