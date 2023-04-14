994b724d15c26fe706f8f565047d6fd68c684651
jahnito <jahnito@gmail.com>
2023-04-14 16:12:32 +0500

    Изменения в схеме БД добавлена таблица технической возможности, изменена таблица address базовая часть страницы вынесена в отдельны шаблон  base.html добавлена функция сормирования словаря тех возможности и передачи в шаблон

--- main.py
+++ main.py
@@ -7,34 +7,56 @@ from sys import platform
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 # Тут мелкая боль разработки под виндой, хех, путь коварен.
+# Переходи на светлую сторону, ставь Linux!
 if platform == 'win32':
     DB = os.getcwd()+'\\db\\nettab.db'
 else:
     DB = 'db/nettab.db'
 
+
+def tech_possible(db: str) -> dict:
+    '''
+    Функция возвращает словарь где ключ объект/адрес(id) хранятся кортежи (услуга, тип подключения)
+    '''
+    result = {}
+    connection = sqlite3.connect(db)
+    cursor = connection.cursor()
+    cursor.execute("""SELECT technical_possibility.object_id, technical_possibility.service, type_phys_channel.name
+                    FROM
+                    technical_possibility, type_phys_channel
+                    WHERE
+                    type_phys_channel.id = technical_possibility.type_phys_channel""")
+    for id, service, type in cursor:
+        if result.get(id):
+            result[id].append((service, type))
+        else:
+            result[id] = [(service, type)]
+    return result
+
+
 def get_addresses(db: str) -> list:
     '''
-    Все адреса
+    Вывод всех адресов на главной странице
     '''
     connection = sqlite3.connect(db)
     cursor = connection.cursor()
-    cursor.execute("""SELECT address.root_to, address.city, address.street, address.building, type_channel.name, type_phys_channel.name
-from
-main, address, type_channel, type_phys_channel
-where
-main.address_id = address.id
-AND
-main.type_channel_id = type_channel.id
-AND
-main.type_phys_channel_id = type_phys_channel.id""")
-    return [i for i in cursor]
+    cursor.execute("""SELECT region.name, address.area, address.city, address.street, address.building, areas.area_name, address.id
+                    FROM
+                    address, areas, region
+					WHERE
+					address.area_id = areas.id
+					AND
+					region.id = address.region_id""")
+    return [ i for i in cursor ]
 
 
 @app.route('/')
 def index():
+    tech_pos = tech_possible(DB)
     addresses = get_addresses(DB)
-    return render_template('index.html', addresses=addresses)
+    return render_template('index.html', addresses=addresses, tech_pos=tech_pos)
 
 
 app.run(host='127.0.0.1', port=8080)
--- setup/chaneltab.db.sql
+++ setup/chaneltab.db.sql
@@ -1,28 +1,48 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "region" (
-	"id"	INTEGER,
-	"name"	TEXT
-);
+
 CREATE TABLE IF NOT EXISTS "address" (
-	"id"	INTEGER,
-	"root_to"	TEXT,
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"area"	TEXT,
 	"city"	TEXT,
 	"street"	TEXT,
 	"building"	TEXT,
-	"region_id"	INTEGER
+	"region_id"	INTEGER,
+	"area_id"	INTEGER
 );
-CREATE TABLE IF NOT EXISTS "type_phys_channel" (
-	"id"	INTEGER,
-	"name"	TEXT
+
+CREATE TABLE IF NOT EXISTS "areas" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"area_name"	TEXT UNIQUE
 );
-CREATE TABLE IF NOT EXISTS "type_channel" (
-	"id"	INTEGER,
-	"name"	TEXT
-);
-CREATE TABLE IF NOT EXISTS "main" (
+
+CREATE TABLE "main" (
 	"id"	INTEGER,
 	"address_id"	INTEGER,
 	"type_channel_id"	INTEGER,
 	"type_phys_channel_id"	INTEGER
 );
+
+CREATE TABLE "region" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"name"	TEXT UNIQUE
+);
+
+CREATE TABLE "technical_possibility" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"service"	TEXT,
+	"object_id"	INTEGER,
+	"type_channel"	INTEGER,
+	"type_phys_channel"	INTEGER
+);
+
+CREATE TABLE "type_channel" (
+	"id"	INTEGER,
+	"name"	TEXT
+);
+
+CREATE TABLE "type_phys_channel" (
+	"id"	INTEGER,
+	"name"	TEXT
+);
+
 COMMIT;
