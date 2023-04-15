66cd913b91785c75ae3d203baa74bf67e1026814
jahnito <jahnito@gmail.com>
2023-04-16 01:14:34 +0500

    Add lib flask-wtf Changed requirements Created forms class and rendered in template add_object.html without action

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-#coding:utf8
-
-
+# coding:utf8
 import sqlite3
 import os
 from sys import platform
 from flask import Flask, render_template
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, InputRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0b194d12d2d207912b454db5e529522763433da5676512ba5e3400f37c7b9585'
 
 # Тут мелкая боль разработки под виндой, хех, путь коварен.
 # Переходи на светлую сторону, ставь Linux!
@@ -14,6 +17,16 @@ if platform == 'win32':
     DB = os.getcwd()+'\\db\\nettab.db'
 else:
     DB = 'db/nettab.db'
+
+
+class AddAddressForm(FlaskForm):
+    area = StringField("Район: ")
+    city = StringField("Населенный пункт: ")
+    street = StringField("Улица: ")
+    building = StringField("Номер строения/корпус/литер: ")
+    region = SelectField("Регион: ", choices=[('1', 'Пермский край'), ('2', 'Тюменская область')])
+    area_id = SelectField("Юрисдикция / Принадлежность: ", choices=[('8', 'Свердловский район'), ('17', 'Александровский район')])
+    submit = SubmitField("Загрузить")
 
 
 def tech_possible(db: str) -> dict:
@@ -61,7 +74,9 @@ def index():
 
 @app.route('/add_object.html')
 def add_object():
-    return render_template('add_object.html')
+    form = AddAddressForm()
+
+    return render_template('add_object.html', form=form)
 
 
 app.run(host='127.0.0.1', port=8080)
